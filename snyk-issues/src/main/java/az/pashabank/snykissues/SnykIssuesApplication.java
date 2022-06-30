d216c94189fd94bc95b50c28cd4b71837e4ccde1
IKT-Lab <nadircabbarli689@gmail.com>
2022-06-30 14:37:14 +0400

    Initial commit

---
+++ snyk-issues/src/main/java/az/pashabank/snykissues/SnykIssuesApplication.java
@@ -0,0 +1,13 @@
+package az.pashabank.snykissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnykIssuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnykIssuesApplication.class, args);
+	}
+
+}
---
+++ snyk-issues/src/test/java/az/pashabank/snykissues/SnykIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package az.pashabank.snykissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnykIssuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
